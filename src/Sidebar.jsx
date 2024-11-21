62a26223bf6ea38f83dda1faef1d4a04e47a0ef4
DiegoRojo99 <diego99rojo@gmail.com>
2024-11-21 22:54:00 +0000

    Added GitHub and LinkedIn

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './Portfolio.css';
 import Avatar from './img/Avatar.jpeg';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare, faBriefcase, faCode } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 function Sidebar(){
   return (
     <div className="sidebar">
@@ -14,10 +18,37 @@ function Sidebar(){
       </div>
       <nav>
         <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li>
+            <a href="#experience">
+              <FontAwesomeIcon className='sidebar-icon' icon={faBriefcase} />
+              <span className='nav-title'>Experience</span>
+            </a>
+          </li>
+          <li>
+            <a href="#projects">
+              <FontAwesomeIcon className='sidebar-icon' icon={faCode} />
+              <span className='nav-title'>Projects</span>
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <span>Contact</span>
+          <li>
+            <a href="https://github.com/DiegoRojo99">
+              <FontAwesomeIcon className='sidebar-icon' icon={faGithub} />
+              <span className='nav-title'>GitHub</span>
+              {/* <FontAwesomeIcon className='external-link' icon={faArrowUpRightFromSquare} /> */}
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/diego-rojo-a62229184">
+              <FontAwesomeIcon className='sidebar-icon' icon={faLinkedin} />
+              <span className='nav-title'>LinkedIn</span>
+              {/* <div className='link-container'>
+                <FontAwesomeIcon className='external-link' icon={faArrowUpRightFromSquare} />
+              </div> */}
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
