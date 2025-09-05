a70834360633e1de08de3e593dae0b8276f4ad34
DiegoRojo99 <diego99rojo@gmail.com>
2025-09-05 16:47:07 +0200

    Items have bg color now

--- src/main/Skills.jsx
+++ src/main/Skills.jsx
@@ -6,17 +6,17 @@ const mainSkills = [
   { name: "Javascript", image: 'skills/Javascript.png' },
   { name: "Node.js", image: 'skills/Node.png' },
   { name: "Next.js", image: 'skills/next-js.png' },
-  { name: "MySQL", image: 'skills/MySQL.png' },
-  { name: "PostgreSQL", image: 'skills/Postgresql.png' },
   { name: "CSS", image: 'skills/CSS.svg' },
   { name: "HTML", image: 'skills/HTML.png' },
-  { name: "Prisma", image: 'skills/Prisma.png' },
   { name: "Express.js", image: 'skills/Express.png' },
+  { name: "Firebase", image: 'skills/Firebase.png' },
+  { name: "MySQL", image: 'skills/MySQL.png' },
+  { name: "PostgreSQL", image: 'skills/Postgresql.png' },
+  { name: "Prisma", image: 'skills/Prisma.png' },
 ]
 
 const otherSkills = [
   { name: "Java", image: 'skills/Java.png' },
-  { name: "Firebase", image: 'skills/Firebase.png' },
   { name: "Python", image: 'skills/Python.png' },
   { name: "R", image: 'skills/R.png' },
   { name: "PHP", image: 'skills/PHP.png' },
@@ -46,14 +46,14 @@ function Skills(){
       <h4>Languages</h4>
       <div className="skills-grid">
         <div className="skills-item">
-          <img className='flag' alt="English" src={`/img/English.png`} />  
-          <p className='skills-name'>English</p>
-          <p className='language-title'>C1</p>
-        </div>
-        <div className="skills-item">
           <img className='flag' alt="Spanish" src={`/img/Spanish.png`} />
           <p className='skills-name'>Spanish</p>
           <p className='language-title'>Native</p>
+        </div>
+        <div className="skills-item">
+          <img className='flag' alt="English" src={`/img/English.png`} />  
+          <p className='skills-name'>English</p>
+          <p className='language-title'>C1</p>
         </div>
         <div className="skills-item">
           <img className='flag' alt="French" src={`/img/French.png`} />
