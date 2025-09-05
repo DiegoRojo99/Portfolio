456e20d7542fb32d19e88c4a4040cab1e29097cb
DiegoRojo99 <diego99rojo@gmail.com>
2025-09-05 16:46:46 +0200

    Updated projects

--- src/main/Projects.jsx
+++ src/main/Projects.jsx
@@ -5,6 +5,14 @@ import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const projects = [
   {
+    name: "FM Tracker",
+    tech: ["React", "Typescript", "CSS", "Node.js", "Express.js", "Firebase", "Firestore"],
+    website: "https://fm-tracker.xyz/",
+    github: "https://github.com/DiegoRojo99/fm-tracker",
+    description: 'A Football Manager companion app to track your saves and challenges.',
+    image: 'FMTracker.png'
+  },
+  {
     name: "Park Hopper",
     tech: ["React", "Typescript", "Javascript", "Node.js", "CSS", "MySQL", "Firebase", "Express.js"],
     // website: "https://parkhopper.live",
@@ -12,27 +20,27 @@ const projects = [
     description: 'Allows a user to keep track of theme park attractions waiting times and set up alerts.',
     image: 'ParkHopper.png'
   },
-  {
-    name: 'The Match Diary',
-    tech: ["Next.js", "React", "Typescript", "CSS", "Node.js", "Express.js", "PostgreSQL", "Prisma"],
-    website: "https://the-match-diary.vercel.app/",
-    github: "https://github.com/DiegoRojo99/The-Match-Diary",
-    description: 'A platform to track all your football visits.',
-    image: 'TheMatchDiary.png'
-  },
-  {
-    name: "The Blockbuster Arena",
-    tech: ["React", "Typescript", "CSS"],
-    website: "https://the-blockbuster-arena.netlify.app/",
-    github: "https://github.com/DiegoRojo99/The-Blockbuster-Arena",
-    description: 'Different movie based games including guessing movies and actors based on hints.',
-    image: 'TheBlockbusterArena.png'
-  },
+  // {
+  //   name: 'The Match Diary',
+  //   tech: ["Next.js", "React", "Typescript", "CSS", "Node.js", "Express.js", "PostgreSQL", "Prisma"],
+  //   website: "https://the-match-diary.vercel.app/",
+  //   github: "https://github.com/DiegoRojo99/The-Match-Diary",
+  //   description: 'A platform to track all your football visits.',
+  //   image: 'TheMatchDiary.png'
+  // },
+  // {
+  //   name: "The Blockbuster Arena",
+  //   tech: ["React", "Typescript", "CSS"],
+  //   website: "https://the-blockbuster-arena.netlify.app/",
+  //   github: "https://github.com/DiegoRojo99/The-Blockbuster-Arena",
+  //   description: 'Different movie based games including guessing movies and actors based on hints.',
+  //   image: 'TheBlockbusterArena.png'
+  // },
   {
     name: "Play Hive",
-    tech: ["Typescript", "React", "CSS", "Node.js", "Express.js", "PostgreSQL", "Prisma"],
+    tech: ["Typescript", "React", "Vite", "CSS", "Node.js", "Express.js"],
     website: "https://play-hive.vercel.app/",
-    github: "https://github.com/DiegoRojo99/Play-Hive",
+    github: "https://github.com/DiegoRojo99/game-hive",
     description: 'Your ultimate companion for tracking and enhancing your gaming experience.',
     image: 'PlayHive.png'
   },
@@ -56,7 +64,7 @@ function ProjectSkillsItem({item}){
 
 function ProjectItem({project}){
   return (
-    <div className="project-item">
+    <div className="project-item contrast">
       <img className='project-cover' src={project.image ? require(`../img/${project.image}`) : ''} alt={project.name} />
       <div className='project-content'>
         <h3 className='project-title'>{project.name}</h3>
